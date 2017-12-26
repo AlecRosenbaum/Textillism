2463fd51800e95cb3f6b1262f5a6d738b7d03cfa
Alec Rosenbaum <alecr95@gmail.com>
2017-12-25 22:22:12 -0500

    refine sample + create readme

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Textillism
-Create an image using text
+
+![sample input](sample/input_image.jpg "Sample Input") -> ![sample output](sample/output.jpg "Sample Output")
+
+Create an image using text! Convert a source image into a black-and-white rendition comprised entirely of the input text. Here's how to use this script:
+
+* Install requirements:
+    - `pip install -r requirements.txt`
+* Modify script to use custom images/text/font/etc
+    - ```
+    INPUT = "sample/input_image.jpg"
+    TEXT_FILE = "sample/input_text.txt"
+    OUTPUT_FILE = "sample/output.jpg"
+    OUTPUT_SCALE = 2
+    FONT_NAME = "sample/Arial_Bold.ttf"
+    FONT_SIZE = 10
+    ```
+* Run script: `python main.py`
+
+Note that depending on the scale + input image size, it make take a little while to calculate. On 400x400 source images with OUTPUT_SCALE = 2, it takes around 15-20 seconds on my 12" MacBook.
--- main.py
+++ main.py
@@ -3,20 +3,20 @@ from math import ceil
 from PIL import Image, ImageFont, ImageDraw
 import numpy as np
 
-# specified constants
-INPUT = "input_image/christmas_tree.jpg"  # hardcoded for now
-TEXT_FILE = "bee_movie.txt"
-OUTPUT_FILE = "christmas_tree.jpg"
+# specified constants - MODIFY THESE
+INPUT = "sample/input_image.jpg"
+TEXT_FILE = "sample/input_text.txt"
+OUTPUT_FILE = "sample/output.jpg"
 OUTPUT_SCALE = 2
-FONT_NAME = "Arial_Bold.ttf"
+FONT_NAME = "sample/Arial_Bold.ttf"
 FONT_SIZE = 10
 
 # derived constants
 INPUT_IMAGE = Image.open(INPUT)
 INPUT_DIMENSIONS = INPUT_IMAGE.size
 OUTPUT_DIMENSIONS = (INPUT_DIMENSIONS[0] * OUTPUT_SCALE, INPUT_DIMENSIONS[1] * OUTPUT_SCALE)
-FONT = ImageFont.truetype("fonts/" + FONT_NAME, FONT_SIZE)
-with open("input_text/" + TEXT_FILE) as fin:
+FONT = ImageFont.truetype(FONT_NAME, FONT_SIZE)
+with open(TEXT_FILE) as fin:
     TEXT = fin.read().replace("\n", "").replace(" ", "")
 
 
@@ -31,7 +31,7 @@ def main():
     x = 0
     cnt = 0
     while y < OUTPUT_DIMENSIONS[1] - FONT_SIZE:
-        # turn output y -> input y
+        # map current y -> input y
         input_y = int(y/OUTPUT_SCALE)
 
         # find optimal spacing between min and max bounds
@@ -43,15 +43,15 @@ def main():
             img = img.crop(box=(0, 2, img.width, img.height))
 
             # calculate brightness
+            # Note: I experimented with a few different measures, but this seemed
+            #       to always produce the best results
             calc_brightness = (np.mean(np.array(img)) - 128) * 2
-            # calc_brightness = np.mean(np.array(img))
-            # calc_brightness = ImageStat.Stat(img).rms[0]
 
-            # target brightnexx
-            target_brightness = np.mean(input_vals[input_y:input_y+ceil(img.height/OUTPUT_SCALE), int(x/OUTPUT_SCALE):int(x/OUTPUT_SCALE)+ceil(img.width/OUTPUT_SCALE)])
-            # print("x:", x, "y:", y, "->", int(x/OUTPUT_SCALE), ":", ceil(img.width/OUTPUT_SCALE), ", ", input_y, ":", input_y+ceil(img.height/OUTPUT_SCALE))
+            # calculate target brightness
+            target_brightness = np.mean(input_vals[
+                    input_y: input_y+ceil(img.height/OUTPUT_SCALE),
+                    int(x/OUTPUT_SCALE): int(x/OUTPUT_SCALE) + ceil(img.width/OUTPUT_SCALE)])
 
-            # print("spacing", i, "calculated:", calc_brightness, "target:", target_brightness)
             spacing.append((abs(target_brightness - calc_brightness), img))
 
         _, img = min(spacing, key=lambda x: x[0])
@@ -66,9 +66,7 @@ def main():
             y += img.size[1]
 
     img = Image.fromarray(img_vals, mode='L')
-    # if OUTPUT_FILE is None:
     img.show()
-    # else:
     img.save(OUTPUT_FILE)
 
 
